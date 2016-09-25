c02645618f86c2eee960b08cbabe9a93afe1d4d1
Nik Coughlin <nik@mojule.co.nz>
2016-09-26 10:06:38 +1300

    formatting

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,8 @@
 const gulp = require( 'gulp' )
 const babel = require( 'gulp-babel' )
 
-gulp.task( 'default', () => {
-  return gulp.src( 'src/**/*.js' )
-    .pipe( babel({
-      presets: [ 'es2015' ]
-    }))
+gulp.task( 'default', () =>
+  gulp.src( 'src/**/*.js' )
+    .pipe( babel( { presets: [ 'es2015' ] } ) )
     .pipe( gulp.dest( 'dist' ) )
-})
+)
